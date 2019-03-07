370d3900f802b6db4e25fe7939de30a9805fcc38
Eduard <eduard66_06@list.ru>
2019-03-07 13:46:12 +0500

    fix basic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-'use strict'
+"use strict"
 
 var button = document.querySelector(".form-title__button");
 
 var modalOrder = document.querySelector(".modal--order");
 
+modalOrder.classList.remove("modal--nojs");
 
 button.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    modalOrder.classList.toggle("modal-show");
+  evt.preventDefault();
+  modalOrder.classList.toggle("modal--show");
 });
